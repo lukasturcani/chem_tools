a07ac8e83a717bd945cf87e29610e3d5dff6ae1b
Lukas Turcani <lukasturcani93@gmail.com>
2018-04-25 19:39:38 +0100

    added support for saving building blocks

--- update_database/input_files/add_structure.py
+++ update_database/input_files/add_structure.py
@@ -23,11 +23,15 @@ def mongo(molecule, key):
     return {
         '$set': {'key': key(molecule)},
 
-        '$push': {'structures': {
+        '$push': {
+                  'tags': {'$each': []},
+                  'structures': {
                        'structure': molecule.mdl_mol_block(),
-                       'calc_params': {'software': 'schrodinger2017-4',
+                       'calc_params': {
+                                       'software': 'schrodinger2017-4',
                                        'max_iter': 5000,
-                                       'md': {'confs': 50,
+                                       'md': {
+                                              'confs': 50,
                                               'temp': 700,
                                               'sim_time': 2000,
                                               'time_step': 1.0,
@@ -35,8 +39,13 @@ def mongo(molecule, key):
                                               'max_iter': 2500,
                                               'eq_time': 100,
                                               'gradient': 0.05,
-                                              'timeout': 0},
+                                              'timeout': 0
+                                              },
                                        'force_field': 16,
                                        'restricted': 'both',
                                        'timeout': 0,
-                                       'gradient': 0.05}}}}
+                                       'gradient': 0.05
+                                      }
+                                  }
+                   }
+                 }
--- update_database/update_database.py
+++ update_database/update_database.py
@@ -10,6 +10,32 @@ from collections import OrderedDict
 import numpy as np
 
 
+def mongo_bb(bb):
+    """
+    Creates a :class:`dict` to MongoDB with a building block.
+
+    Parameters
+    ----------
+    bb : :class:`stk.Structunit`
+        The building block
+
+    Returns
+    -------
+    :class:`dict`
+        A :class:`dict` which updates MongoDB with `bb` info.
+
+    """
+
+    return {
+            '$setOnInsert': {
+                              'inchi': bb.inchi,
+                              'fg': bb.func_grp.name,
+                              'num_fgs': len(bb.functional_group_atoms()),
+                              'structure': bb.mdl_mol_block()
+                              }
+           }
+
+
 def cage_key(topology):
     """
     Creates a key for :class:`stk.CageTopology`.
@@ -94,7 +120,9 @@ def key(molecule):
 
 
 def main():
-    parser = argparse.ArgumentParser()
+    parser = argparse.ArgumentParser(description=(
+           'Any building block will be placed in the "bbs" '
+           'database and "main" collection.'))
     parser.add_argument('mongo_uri', help='URI to the MongoDB server.')
     parser.add_argument('db', help='Name of the database.')
     parser.add_argument('collection', help='Name of the collection.')
@@ -111,10 +139,13 @@ def main():
                         help=('An stk Population JSON dump file. '
                               'The molecules stored in the file will '
                               'be saved in the MongoDB.'))
+    parser.add_argument('-s', '--save_bbs', action='store_true')
 
     args = parser.parse_args()
 
-    col = pymongo.MongoClient(args.mongo_uri)[args.db][args.collection]
+    client = pymongo.MongoClient(args.mongo_uri)
+    col = client[args.db][args.collection]
+    bbs = client.bbs.main
 
     p = stk.Population()
     for pop_file in args.population_file:
@@ -126,6 +157,9 @@ def main():
 
     for mol in p:
         col.update_one(key(mol), input_file['mongo'](mol, key), True)
+        if args.save_bbs:
+            for bb in mol.building_blocks:
+                bbs.update_one({'inchi': bb.inchi}, mongo_bb(bb), True)
 
 
 if __name__ == '__main__':
